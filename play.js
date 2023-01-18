fce657ebc0e7e299fdb75c2aa2d026472b0684d3
Lauren Johnston <hilotraining@outlook.com>
2023-01-18 14:49:51 -0700

    added keyboard listener to play.js

--- play.js
+++ play.js
@@ -5,3 +5,23 @@
 "Move: right" - move left one square (unless facing left)*/ 
 //Imports player connection
 const player = require("./client");
+
+
+// stdin object will allow us to listen for keyboard input and react to it.
+const setupInput = () => {
+  const stdin = process.stdin; //The stdin property of the process object is a Readable Stream. It uses on() function to listen for the event.
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //register an event listener for stdin
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+ const handleUserInput = (key) => {
+   if (key === '\u0003') {
+  process.exit();
+}
+ };
